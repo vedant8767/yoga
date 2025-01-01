7616c323f6cbb364d24886ad7587b04a436616d8
vedant8767 <155797741+vedant8767@users.noreply.github.com>
2025-01-01 17:14:32 +0530

    changes

--- src/components/Home/Testimonial.jsx
+++ src/components/Home/Testimonial.jsx
@@ -1,33 +1,7 @@
-// import React from 'react'
-// import testimonial from '../assets/testimonial.png'
-// import '../css/Testimonial.css'
 
-// function Testimonial() {
-//   return (
-//     <div className='testimonial'>
-//       <div className="top_testimonial">
-//         <h1>TESTIMONIALS</h1>
-//         <h2>what people are saying</h2>
-//       </div>
-//       <div className='testimonial_content'>
-//         <p>
-//         <span>“</span> The yoga classes help me to relax and de-stress, and I always leave 
-//         feeling refreshed and energized. I highly recommend 8 Limbs Yoga 
-//         to anyone who is feeling stressed or overwhelmed. <span>”</span>
-//         </p>
-//       </div>
-//       <div className="testmonialinfo">
-//         <img src={testimonial} alt="" srcset="" />
-//         <p>Aashu yadav</p>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Testimonial
 import React from "react";
-import Slider from "react-slick"; // Import React Slick
-import "../../css/Testimonial.css"; // Import CSS for styling
+import Slider from "react-slick"; 
+import "../../css/Testimonial.css"; 
 
 const Testimonial = () => {
   const settings = {
