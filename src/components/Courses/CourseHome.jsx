d6e4626af6e888deae84f1317b0c640963952eaa
vedant8767 <155797741+vedant8767@users.noreply.github.com>
2025-01-02 13:59:00 +0530

    font

--- src/components/Courses/CourseHome.jsx
+++ src/components/Courses/CourseHome.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../css/CourseHome.css'
+import  CaurselCompnent from '../CarouselComponent'
 
 function CourseHome() {
   return (
@@ -13,9 +14,10 @@ function CourseHome() {
                 <button id='cbt2'>View Similar Programs</button>
             </div>
         </div>
-        <div className="coursehoemright" data-aos="zoom-in">
+        <CaurselCompnent/>
+        {/* <div className="coursehoemright" data-aos="zoom-in">
             <img src="https://s3-alpha-sig.figma.com/img/7f35/bad6/2a1c5403801fc1e23d7b0017313d583a?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=LE2tUS9IInWx6MLFbu7KDM41BHJ4DHxO~SjrK7FoWFQpY5Qj2SUYwVIDsZfT7D2bayLhyinCsww1vP9J2gI~V2GGuv6kLPo3fMzLruBX7sQ6yI4QPPeGnsHOi1yTSgMoTz7kzkJvit8Jp50ZJzItepLwtTCYS36bBgbXoIXISOx0vQ5gjp1earPzky8JdVRrM~z~-JIttS~F6W8PwmaQ5~DH4waiZVrxhTW08g3JPm1sdYmL-EPQkYuBw3NYWJjH-gI1EaGz45H3hjoQtSSarpiDuuNhcylOWE~221gM28v-StBLQOSlfn5GDSH2Y0WqQnTs42tcws9G6Bw-QLRexw__" alt="" srcset="" />
-        </div>
+        </div> */}
     </div>
   )
 }
--- src/components/Courses/CourseindDetails.jsx
+++ src/components/Courses/CourseindDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import '../../css/CourseindDetails.css';
 import { CSSTransition, SwitchTransition } from "react-transition-group";
+import CarouselComponent from "../CarouselComponent";
 
 const CourseindDetails = () => {
   const [activeSection, setActiveSection] = useState("Program details");
@@ -18,7 +19,9 @@ const CourseindDetails = () => {
                 <p>The course is taught by a team of experienced and certified yoga teachers who are passionate about sharing their knowledge of yoga with students. They cover various styles of yoga, including Hatha, Ashtanga, Vinyasa, and Iyengar, providing students with a well-rounded education in yoga.</p>
                 <p>The course curriculum includes asana practice, pranayama, meditation, yoga philosophy, anatomy, and teaching methodology. The classes are kept small to ensure that students receive personalized attention and guidance from the teachers.</p>
             </div>
-            <div className="crindcontentright">
+            <CarouselComponent/>
+            {/* <CarouselComponent/> */}
+            {/* <div className="crindcontentright">
                 <div className="rimg">
                     <img src="https://s3-alpha-sig.figma.com/img/461b/9642/dd5978042b517d576c03aa2eaa92efcd?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ewisIbfV9e94wLf0zBRkdwxirHLyQKOPeeEA3N1i~QahelkEYa2uGRKbSfNUY0JWF3TiInHFBRxXGXtpjmDWVHZFnow72dan5BmF3LS5aHAQi8s18BQX7GOWptJM14Mu~H~AHHkxQjqtB8FCO7ThyBncrXpEJlk9Vf~G0YDFa3eL1hl7iC7xB-~6mV3YrJHI-Aj72WlVFlR06x0erkoHKlqMz7E~7g0V34-5xuf4464B~zFYGU0sg5-8x-ej6yr6SVuu8Zauc2J9hV7Qin78GwI151WMcz4~z7J3Ms8AikCpAghqEfQXy6Wh-sb~6AOQuXUb8mUO42Uh2IeJLaGAVQ__" alt="" />
                     <img src="https://s3-alpha-sig.figma.com/img/0c43/062b/9e1637f95ae7a6e126297d9730aca3f9?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NG~fJYPbli2yHrnT~8oBSFLQ88mBhxZ6LxaZj~O7AJ5ty90txFbhc7BzK5MCr9rcgbVnLHClYKI9VIh9g9IcadZZR-dPMpKZGihjVSTt~DcRQ6-B4OMyKguYmxEfU50C7C8-G3zX87ff3B2z9jJ1pOFpQea5Xe8U9ht1JLTc7zKTo5tNfJK4Ilcc1EfhVzhIjWrx1j7fMBnNAT9ca-INMs4U75weS2iz-g~ArmQSO-XEs77eM1ax-9az5jkw3oDXtHrHDXl323oMfBe4Z8owK1Q5gmETCKUyG2TFkbsPjKlgLufnmMnLNng99hEVuEHQqD1qjXLs0aewcCGKnu16VA__" alt="" />
@@ -26,7 +29,7 @@ const CourseindDetails = () => {
                 <div className="rleft">
                     <img src="https://s3-alpha-sig.figma.com/img/d8fe/a8d0/c89acac9cf757739edb3baad69f5269a?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=agLivp0TkLSEQjY~AwtrlU~ECzwhvYA~LB62fDmqA01Mq4gYJnM~QkWN3nGVGqGMnAoSMWydo7YeS0gBBD43qJrkPG6sxh8WS7SqQJvyFXd4GWPRmetS7L0iogOAvfpyZ0dQ9TXKwqYZODl6MISJcvQ-urhhPqWBnYPtnpE4BOWC0FgUZp97ZOekBOuy8ClB2~hvOFMwoNvJdXyiwhFdc07vURJ4HiDqvQZ1Hbu0iAcjwmaTsCvYYxSq~VqazlN~kiB6Jp-iEsYtQsW8xizJ78CblOhDZRLMSwSOxyEon4b--IUkvgMRJfFl1a09-1V~hzJtdMrRhsdW4XoluYv81g__" alt="" srcset="" />
                 </div>
-            </div>
+            </div> */}
         </div>
      </div>
     ),
