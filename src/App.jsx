41f6eb84e48308cacfa6d534d35b58d735a6e7cd
vedant8767 <155797741+vedant8767@users.noreply.github.com>
2025-01-03 22:15:12 +0530

    allfinal

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import Faq from './components/Faq/Faq'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
-import HomePage from './pages/HomePage'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { Outlet } from 'react-router-dom'
@@ -11,14 +10,12 @@ import { Outlet } from 'react-router-dom'
 function App() {
   useEffect(()=>{
     AOS.init({
-        // Add options here, if needed
-        duration: 1500, // Animation duration in milliseconds
-        once: true, // Whether animation should happen only once or every time you scroll
+        duration: 1500, 
+        once: true,
       });
 },[])
   return (
     <div>
-      {/* <HomePage/> */}
       <Navbar/>
       <Outlet/>
       <Faq/>
